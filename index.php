2491a694960c8c929aa875a96efc78576c284967
Tor Bjurbom <34156187+Bjurbom@users.noreply.github.com>
2018-09-07 11:04:09 +0200

    preparing for css

--- index.php
+++ index.php
@@ -3,11 +3,24 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Page Title</title>
+    <title>Välkomen till tvättstugan</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
 </head>
 <body>
+<div id="leftContainer"> 
+</div>
+    <div id="headerContainer">
+
     
+    </div>
+    <div id="mainBodyContainer">
+
+
+
+
+    </div>s
+<div id="rightContainer">
+</div>
 </body>
 </html>
