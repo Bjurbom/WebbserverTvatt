376470096552404c34849581411372b2e6231fdc
Hugo Andersson <Hugganpro123@hotmail.se>
2018-10-03 15:55:59 +0200

    Updated user
    La till ett id i users.

---
+++ users.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Värd: 127.0.0.1
+-- Tid vid skapande: 03 okt 2018 kl 15:55
+-- Serverversion: 10.1.31-MariaDB
+-- PHP-version: 7.2.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Databas: `tvattstugan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellstruktur `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) UNSIGNED NOT NULL,
+  `lagenhetsnummer` int(3) NOT NULL,
+  `losenord` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
+  `bokning` time(6) NOT NULL,
+  `namn` varchar(150) COLLATE utf8_swedish_ci NOT NULL,
+  `adress` varchar(100) COLLATE utf8_swedish_ci NOT NULL,
+  `bild` varchar(500) COLLATE utf8_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_swedish_ci;
+
+--
+-- Dumpning av Data i tabell `users`
+--
+
+INSERT INTO `users` (`id`, `lagenhetsnummer`, `losenord`, `bokning`, `namn`, `adress`, `bild`) VALUES
+(0, 0, '828fd9255753432d51df95eb62d61722', '14:00:00.000000', '0', 'hejvägen 123', ''),
+(1, 1, '828fd9255753432d51df95eb62d61722', '14:00:00.000000', 'Tor Fjaestad', 'hejvägen 123', '');
+
+--
+-- Index för dumpade tabeller
+--
+
+--
+-- Index för tabell `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
