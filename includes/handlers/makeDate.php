ea5ff2b64d6b6812df949675b3b2009f2dbb5ef8
Tor Bjurbom <34156187+Bjurbom@users.noreply.github.com>
2018-10-24 16:14:26 +0200

    boi you can not put the same time as another user
    177013

--- includes/handlers/makeDate.php
+++ includes/handlers/makeDate.php
@@ -5,6 +5,8 @@ $date = $_POST['datum'];
 $time = $_POST['tid'];
 $agare = $_SESSION['agare'];
 
+
+
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 $link = mysqli_connect("localhost", "root", "", "tvattstugan");
@@ -13,7 +15,7 @@ $link = mysqli_connect("localhost", "root", "", "tvattstugan");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
- 
+
 
 $sqll = "SELECT * FROM schemat WHERE agare='$agare'";  
 $result = $link->query($sqll);
@@ -25,18 +27,44 @@ if($result->num_rows == 0){
         
 
         if(date("Y-m-d") >$date){
-            header("Location: ../../user.php");
+       //     header("Location: ../../user.php");
             die();
         }
 
+        $seeIfSomethingIsThere = "SELECT * FROM schemat WHERE dagforbokning='$date'";
 
-        // Attempt insert query execution
-        $sql = "INSERT INTO `schemat` (`id`, `agare`, `tidForBokning`, `dagforbokning`) VALUES (NULL, '$agare', '$time', '$date');";
-    if(mysqli_query($link, $sql)){
-        echo "Records inserted successfully.";
-    } else{
-        echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
-    }
+        $result = $link->query($seeIfSomethingIsThere);
+
+        if($result->num_rows == 1){
+           
+
+            header("Location: ../../user.php");
+            echo("same day ");
+            $seeIfTimeIsSame = "SELECT * FROM schemat WHERE tidForBokning='$time'";
+            $result = $link->query($seeIfTimeIsSame);
+            
+            if($result->num_rows == 1){
+                
+                header("Location: ../../user.php");
+                echo("same time ");
+                die();
+            }else{
+                $sql = "INSERT INTO `schemat` (`id`, `agare`, `tidForBokning`, `dagforbokning`) VALUES (NULL, '$agare', '$time', '$date');";
+                if(mysqli_query($link, $sql)){
+                    echo "Records inserted successfully.";
+                } else{
+                    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+                }
+            }
+        }else{
+            $sql = "INSERT INTO `schemat` (`id`, `agare`, `tidForBokning`, `dagforbokning`) VALUES (NULL, '$agare', '$time', '$date');";
+            if(mysqli_query($link, $sql)){
+                echo "Records inserted successfully.";
+            } else{
+                echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+            }
+        }
+
  
 }
 
--- test.php
+++ test.php
@@ -1,5 +1,26 @@
 <?php
 
+$link = mysqli_connect("localhost", "root", "", "tvattstugan");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+$mkti = mktime(11, 14, 54, 8, 12, 2014);
+
+$time = date("Y-m-d", $mkti);
+
+$seeIfSomethingIsThere = "SELECT * FROM schemat WHERE tidForBokning='$time'";
+
+$result = mysqli_query($link, $seeIfSomethingIsThere);
+
+echo ($result);
+
+ 
+
+
+
 echo date("Y-m-d")
 
 
