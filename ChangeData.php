1c39142ba4f219faf0df3cf9993c6932e5153ab7
Tor Bjurbom <34156187+Bjurbom@users.noreply.github.com>
2018-11-23 10:12:15 +0100

    ändra icon samt på börjat med ändra datan

--- ChangeData.php
+++ ChangeData.php
@@ -11,6 +11,18 @@ if(!$adminId == "0"){
     header("Location: user.php");
 }
 
-
-
 ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Change Data</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    
+</body>
+</html>
